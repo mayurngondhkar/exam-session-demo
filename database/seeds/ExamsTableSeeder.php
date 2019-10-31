b2aea395b169d00704437c8937758682efc7d6a0
Mayur Naresh Gondhkar <mayurngondhkar@gmail.com>
2019-11-01 05:03:23 +1100

    Update Exam Seeder for testing

--- database/seeds/ExamsTableSeeder.php
+++ database/seeds/ExamsTableSeeder.php
@@ -13,6 +13,46 @@ class ExamsTableSeeder extends Seeder
     public function run()
     {
         $exam = new \App\Exam([
+            'code' => '111111',
+            'name' => 'aaaaaa',
+            'start_time' => '2019-11-23 09:00:00',
+            'end_time' => '2019-11-23 12:00:00'
+        ]);
+        $exam->save();
+
+        $exam = new \App\Exam([
+            'code' => '111112',
+            'name' => 'baaaaa',
+            'start_time' => '2019-11-24 09:00:00',
+            'end_time' => '2019-11-24 12:00:00'
+        ]);
+        $exam->save();
+
+        $exam = new \App\Exam([
+            'code' => '111122',
+            'name' => 'bbaaaa',
+            'start_time' => '2019-11-25 09:00:00',
+            'end_time' => '2019-11-25 12:00:00'
+        ]);
+        $exam->save();
+
+        $exam = new \App\Exam([
+            'code' => '111222',
+            'name' => 'bbbaaa',
+            'start_time' => '2019-11-26 09:00:00',
+            'end_time' => '2019-11-26 12:00:00'
+        ]);
+        $exam->save();
+
+        $exam = new \App\Exam([
+            'code' => '112222',
+            'name' => 'bbbbaa',
+            'start_time' => '2019-11-27 09:00:00',
+            'end_time' => '2019-11-27 12:00:00'
+        ]);
+        $exam->save();
+
+        $exam = new \App\Exam([
             'code' => Str::random(8),
             'name' => Str::random(10),
             'start_time' => '2019-11-23 09:00:00',
